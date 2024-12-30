4284aa2d83758e0e4e0968d7eb7ec681066540c9
mouloud78 <mouloudouldali78@gmail.com>
2024-12-29 21:21:34 -0500

    fetch data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import { Product } from "./products";
+
 function App() {
-  return <div style={{ fontSize: "1.2rem" }}>Restore</div>;
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const addProduct = () => {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        id: prevState.length + 1,
+        name: "Product" + (prevState.length + 1),
+        price: prevState.length * 100 + 100,
+        quantityInStock: 100,
+        description: "test",
+        pictureUrl: "https://picsum.photos/200",
+        type: "test",
+        brand: "test",
+      },
+    ]);
+  };
+
+  return (
+    <div>
+      <h1>Re-store</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            <h3>{product.name}</h3>
+            <p>{product.price}</p>
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add product</button>
+    </div>
+  );
 }
 
 export default App;
