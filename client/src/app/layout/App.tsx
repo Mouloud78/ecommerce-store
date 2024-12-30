c77c3a56a60a74b6f0537a34b7eeb19a998abba3
mouloud78 <mouloudouldali78@gmail.com>
2024-12-29 23:41:38 -0500

    add toggle dak mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,11 +1,32 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/products";
 import Catalog from "../../features/catalog/Catalog";
-import { Container } from "@mui/material";
+import {
+  Box,
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
 import NavBar from "./NavBar";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const palletType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: palletType,
+      background: {
+        default: palletType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   useEffect(() => {
     fetch("https://localhost:5001/api/products")
@@ -14,12 +35,23 @@ function App() {
   }, []);
 
   return (
-    <>
-      <NavBar />
-      <Container maxWidth="xl" sx={{ mt: 14 }}>
-        <Catalog products={products} />
-      </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+      <Box
+        sx={{
+          minHeight: "100vh",
+          background: darkMode
+            ? "radial-gradient(circle, #1e3aBa, #111B27)"
+            : "radial-gradient(circle, #baecf9, #f0f9ff)",
+          py: 6,
+        }}
+      >
+        <Container maxWidth="xl" sx={{ mt: 8 }}>
+          <Catalog products={products} />
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
 
